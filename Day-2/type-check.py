4975615f35f1b77114fce071be6557558a66dbb7
alekhya <alekhyakandregula@gmail.com>
2026-03-19 13:08:30 +0000

    type-check

---
+++ Day-2/type-check.py
@@ -0,0 +1,11 @@
+len("12345")
+print(type("Hello"))
+print(type(123))
+print(type(3.145))
+print(type(True))
+
+
+name_of_the_user= input("What is your name? ")
+lengh_of_name= len(name_of_the_user)
+
+print("Number of letters in your name:" + str(lengh_of_name))
