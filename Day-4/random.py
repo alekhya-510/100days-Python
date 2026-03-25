fb999fe6befa4c0c21ac0c4f6b1ac21874808cd0
alekhya <alekhyakandregula@gmail.com>
2026-03-25 12:35:02 +0000

    random

---
+++ Day-4/random.py
@@ -0,0 +1,17 @@
+
+~
+~
+import random
+
+
+
+random_no= random.randint (a=1,b=30 )
+print(random_no)
+
+
+
+random_number_0_to_1= random.random()
+print(random_number_0_to_1)
+
+random_float= random.uniform(a=1,b=10)
+print(random_float)
