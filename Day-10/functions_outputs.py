bbab5bad6f46d97315e4541ff38a519110e95997
alekhya <alekhyakandregula@gmail.com>
2026-04-13 18:16:57 +0100

    Functions with outputs

---
+++ Day-10/functions_outputs.py
@@ -0,0 +1,16 @@
+def format_name(f_name,l_name):
+    formatted_f_name= f_name.title()
+    formatted_l_name= l_name.title()
+    return f"{formatted_f_name} {formatted_l_name}"
+
+print(format_name( "AlekHyA","BheemaRaseTYY"))
+
+def function1(text):
+    return text + text
+
+def function2(text):
+    return text.title()
+
+print(function2(function1("Hello")))
+
+
